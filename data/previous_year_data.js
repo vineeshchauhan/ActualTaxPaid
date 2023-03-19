3cb612637a2004982b86ee8ce239c7c4eb9bca58
Vineesh Chauhan <vineesh.chauhan@ukg.com>
2023-03-19 13:35:14 +0530

    Add ActualTaxPaid to git

---
+++ data/previous_year_data.js
@@ -0,0 +1,16 @@
+var previousYearData = [{
+    "assessmentyear": "2020-2021",
+    "taxableIncome": "14,18,070",
+    "taxLiability": "2,47,438",
+    "taxPerc": "17.4%"
+  }, {
+    "assessmentyear": "2021-2022",
+    "taxableIncome": "18,35,000",
+    "taxLiability": "3,77,520",
+    "taxPerc": "20.5%"
+  },{
+    "assessmentyear": "2022-2023",
+    "taxableIncome": "26,30,430",
+    "taxLiability": "6,01,243",
+    "taxPerc": "22.8%"
+  }];
