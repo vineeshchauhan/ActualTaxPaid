d19b5003d231ca5e77aea625e614c49bc9f43a53
Vineesh Chauhan <vineesh.chauhan@ukg.com>
2023-03-25 13:52:15 +0530

    Updated Page

---
+++ data/taxTransactionApril.js
@@ -0,0 +1,12 @@
+var purchaseAndTaxApril = [
+  {
+      "itemno": 1,
+      "type": "Fuel",
+      "description": "Car Petrol",
+      "price": 2000,
+      "tax": 1000,
+      "bill": "link",
+      "comment": "Dummy Transaction",
+      "total": 1000
+  }
+]
