69b22fb0ba0a9aed5886721614921517f3f3ed87
Vineesh Chauhan <vineesh.chauhan@ukg.com>
2023-03-25 14:00:02 +0530

    Updated Page

---
+++ data/salaryAndTaxTotal.js
@@ -0,0 +1,142 @@
+var salaryAndTaxTotal = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalApril = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalMay = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalJun = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalJul = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalAug = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalSep = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+  
+  var salaryAndTaxTotalOct = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalNov = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalDec = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalJan = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalFeb = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
+
+  var salaryAndTaxTotalMar = [{
+    "totalSalary" : "0.0",
+    "directTax" : "0.0",
+    "directTaxPerc" : "0.0%",
+    "netSalary" : "0.0",
+    "indirectTax" : "0.0",
+    "indirectTaxPerc" : "0.0%",
+    "totalTax" : "0.0",
+    "totalTaxPerc" : "0.0%",
+  }];
